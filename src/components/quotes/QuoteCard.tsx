33af19008e442dd0d801a96be07aeb6401e95946
gonzalogomezdev <gonzalogomezprojects@gmail.com>
2025-09-13 07:57:37 -0300

    refactor: quite una importacion sin utilizar

--- src/components/quotes/QuoteCard.tsx
+++ src/components/quotes/QuoteCard.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent } from "@/components/ui/card";
-import QuoteContent from "./QuoteContent";
 import { Quote } from "lucide-react";
 import QuoteServer from "./QuoteServer";
 
